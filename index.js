92458dc4e71bd565a1c55030f7b453dc3649ac97
Claire Cardie <ckcardie@gmail.com>
2025-03-25 16:48:19 -0400

    deleted problem line

--- index.js
+++ index.js
@@ -84,7 +84,6 @@ window.onclick = function(event) {
   //lines for debugging
   console.log(mostExpensiveSort);
   console.log(leastExpensiveSort);
-  //console.log(sorting_preferences.type);
 
   //if "Least Expensive" button is clicked
   //"sorting_preferences" holds value ""
